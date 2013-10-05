bbbdcab22deb03508a8a8c43cba862c724373c21
paine.hanne@gmail.com <paine.hanne@gmail.com>
2013-10-05 17:17:08 -0400

    About to test the injector

--- scrapey.js
+++ scrapey.js
@@ -2,7 +2,17 @@
     "use strict";
     var id = "UNIQUEISH";
     chrome.contextMenus.create({"title": "Hello, world!", id: id, "contexts":["selection"]});
-    chrome.contextMenus.onClicked.addListener(function () {
-        alert("Hello! Alert!"); 
+    chrome.contextMenus.onClicked.addListener(function (info, tab) {
+        var height = 300; 
+        chrome.windows.get(tab.windowId, null, function (wind) {
+        chrome.windows.create({
+            "url":"Panel.html",
+            "type":"popup",
+            "height": height, 
+            "top": wind.top + wind.height - height,
+            "left": wind.left, 
+            "width": wind.width 
+            });
+            })
     }); 
 }())
